9130a3afe9bea03681814ee209bdd239081f8b53
Bulat-Khasanov <bulat.kasanov@gmail.com>
2023-01-17 20:02:17 +0400

    end setup project environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,30 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync');
-const { reload } = require('browser-sync');
+const pug = require('gulp-pug')
 
 function style () {
     return gulp.src('./scss/**/*.scss')
             .pipe(sass())
             .pipe(gulp.dest('./css'))
+            .pipe(browserSync.stream())
+}
+
+function pugTrans () {
+    return gulp.src('./pug/**/*.pug')
+            .pipe(pug({
+                pretty: true
+            }))
+            .pipe(gulp.dest('./html'))
+            .pipe(browserSync.stream())
+}
+
+function indexPugTrans () {
+    return gulp.src('./pug/index.pug')
+            .pipe(pug({
+                pretty: true
+            }))
+            .pipe(gulp.dest('./'))
             .pipe(browserSync.stream())
 }
 
@@ -18,7 +36,8 @@ function watch () {
         browser: "firefox"
     })
     gulp.watch('./scss/**/*.scss', style);
-    gulp.watch('./*.html').on('change', reload);
+    gulp.watch(['./pug/**/*.pug', '!./pug/index.pug'], pugTrans);
+    gulp.watch('./pug/index.pug', indexPugTrans);
 }
 
 exports.style = style;
